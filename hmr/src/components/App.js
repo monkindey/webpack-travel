547b0ab0bf425651714a16107a2e813b16a69240
monkindey <monkindey@163.com>
2017-08-08 20:59:51 +0800

    format hmr src

--- hmr/src/components/App.js
+++ hmr/src/components/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import styles from './App.css';
 
-const App = () => (
-	<div className={styles.app}>
-		<input type="text" defaultValue="monkindey"/>
-		<h2>Hello, monkindey</h2>
-	</div>
-);
+const App = () =>
+  <div className={styles.app}>
+    <input type="text" defaultValue="monkindey" />
+    <h2>Hello, monkindey</h2>
+  </div>;
 
 export default App;
